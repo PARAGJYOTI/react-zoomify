689e3421cf3784328844159013e42bafa2dd2253
_paragjyoti_bablu <paragjyoti2012@gmail.com>
2016-09-01 02:19:29 +0530

    renamed

--- ReactZoom.js
+++ ReactZoom.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-export default class ImageZoom extends React.Component{
+export default class ReactZoom extends React.Component{
      constructor(){
          super()
          this.state={
